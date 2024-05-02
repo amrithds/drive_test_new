9df858080ba56202350be3a9dd6b845e5067135f
Amrith D S <amrith.ds.gowda@gmail.com>
2024-05-02 14:48:46 +0530

    report module

--- drivetest/admin/settings.py
+++ drivetest/admin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iuz1mwz3yfh4cttpm7j+=rm+#+t5h07k^zsr73z!v)qgqkoca^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
