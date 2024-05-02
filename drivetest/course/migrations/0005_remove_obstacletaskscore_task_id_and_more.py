a3f807dc615342ff9ac524ccf8122a00ff56b397
Amrith D S <amrith.ds.gowda@gmail.com>
2024-05-02 17:13:41 +0530

    task admin

--- drivetest/course/migrations/0005_remove_obstacletaskscore_task_id_and_more.py
+++ drivetest/course/migrations/0005_remove_obstacletaskscore_task_id_and_more.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='obstacletaskscore',
-            name='task_id',
-        ),
         migrations.AddField(
             model_name='obstacletaskscore',
             name='success_task_metrics',
