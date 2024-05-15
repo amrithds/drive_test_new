0e0d56dcc5ade1d695032efbfc265bd6214ad249
Amrith D S <amrith.ds.gowda@gmail.com>
2024-05-15 13:13:47 +0530

    failure message

--- drivetest/course/models/obstacle.py
+++ drivetest/course/models/obstacle.py
@@ -8,7 +8,7 @@ class Obstacle(BaseModel):
     name = models.CharField(max_length=100)
     is_mandatory = models.BooleanField(default=False)
     order = models.IntegerField()
-    start_rf_id = models.CharField(max_length=20, blank=False)
+    start_rf_id = models.CharField(max_length=20, blank=False, unique=True)
     end_rf_id = models.CharField(max_length=20, blank=True)
     audio_file= models.FileField(upload_to ='uploads/', blank=True)
 
