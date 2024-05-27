8b3227b6c7e855e4199c63686388a6e3286069ee
Amrith D S <amrith.ds.gowda@gmail.com>
2024-05-27 13:49:11 +0530

    sound

--- drivetest/app_config/admin.py
+++ drivetest/app_config/admin.py
@@ -3,6 +3,6 @@ from app_config.models import Config
 
 # Register your models here.
 class ConfigAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ["name", "value"]
 
 admin.site.register(Config,ConfigAdmin)
