867f759bf160519cc6c637694548a3b616bd6afe
sindhu <sindhu121196@gmail.com>
2024-07-08 16:20:38 +0530

    final report

--- drivetest/report/views.py
+++ drivetest/report/views.py
@@ -18,6 +18,7 @@ from rest_framework.permissions import IsAuthenticated
 from datetime import datetime
 from django.utils.timezone import make_aware
 from django.utils import timezone
+from django.db.models import Max
 # Create your views here.
 
 @api_view(['GET'])
@@ -96,11 +97,36 @@ class FinalReportViewSet(viewsets.ModelViewSet):
         if course_id:
             queryset = queryset.filter(session__course__id=course_id)
         
-        if from_date and to_date:
+
+        if course_id and from_date and to_date:
+            # Convert from_date and to_date to datetime objects
             from_date_obj = datetime.strptime(from_date, '%Y-%m-%d').date()
             to_date_obj = datetime.strptime(to_date, '%Y-%m-%d').date()
-            start_datetime = timezone.make_aware(datetime.combine(from_date_obj, datetime.min.time()))
-            end_datetime = timezone.make_aware(datetime.combine(to_date_obj, datetime.max.time()))
-            queryset = queryset.filter(session__created_at__range=(start_datetime, end_datetime))
 
+            # Define start_datetime and end_datetime for the range filter
+            start_datetime = datetime.combine(from_date_obj, datetime.min.time()).replace(tzinfo=timezone.utc)
+            end_datetime = datetime.combine(to_date_obj, datetime.max.time()).replace(tzinfo=timezone.utc)
+
+
+            latest_sessions = Session.objects.filter(
+                course_id=course_id,
+                created_at__range=(start_datetime, end_datetime),
+            ).values('trainee_id').annotate(
+                max_created_at=Max('created_at')
+            )
+
+            # Build a list of session IDs for the latest sessions
+            session_ids = []
+            for session in latest_sessions:
+                trainee_id = session['trainee_id']
+                max_created_at = session['max_created_at']
+                session_id = Session.objects.filter(
+                    trainee_id=trainee_id,
+                    created_at=max_created_at
+                ).values_list('id', flat=True).first()
+                if session_id:
+                    session_ids.append(session_id)
+           
+            queryset = queryset.filter(session_id__in=session_ids)
+            
         return queryset
