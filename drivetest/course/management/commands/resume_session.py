00b53f1f5391388210c043a014b173f2309ca0ca
sindhu <sindhu121196@gmail.com>
2024-07-12 10:10:39 +0530

    resume session code

--- drivetest/course/management/commands/resume_session.py
+++ drivetest/course/management/commands/resume_session.py
@@ -5,22 +5,23 @@ from course.helper import process_helper
 import shlex
 import subprocess
 import logging
-logger = logging.getLogger("reportLog")
+import time
 
+logger = logging.getLogger("reportLog")
 class Command(BaseCommand):
-    help = "Closes the specified poll for voting"
+    help = "Resumes the session with the specified criteria"
 
     def handle(self, *args, **options):
-        session = Session.objects.filter(~Q(status=Session.STATUS_COMPELETED)).order_by('-created_at').first()
-
-        if session:
-            if not process_helper.is_process_active(session.pid):
-                command = shlex.split(f'python manage.py start_session -i {session.trainer_id} -s {session.trainee_id} -ses {session.id} -m {session.mode} -r 1')
-
-                p = subprocess.Popen(command)
-
-                session.pid = p.pid
-                session.save()
+        while True:
+            logger.info("Resuming session")
+            session = Session.objects.filter(~Q(status=Session.STATUS_COMPELETED)).order_by('-created_at').first()
+            if session:
+                if not process_helper.is_process_active(session.pid):
+                    command = shlex.split(f'python manage.py start_session -i {session.trainer_id} -s {session.trainee_id} -ses {session.id} -m {session.mode} -r 1')
+                    p = subprocess.Popen(command)
+                    session.pid = p.pid
+                    session.save()
+            time.sleep(10)
 
 
 
