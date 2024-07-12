492760c2810a6b6546c5e5899485b135312480c1
sindhu <sindhu121196@gmail.com>
2024-07-12 17:10:53 +0530

    resume

--- server/resume_session.sh
+++ server/resume_session.sh
@@ -2,7 +2,7 @@
 
 echo "Last reboot time:" >> /home/admin/log.log
 
-cd /home/admin/Desktop/app/drive_test
+cd /home/admin/driving_project
 source venv/bin/activate
 cd drivetest
 python manage.py resume_session
