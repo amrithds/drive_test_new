07d4e57bf926fd27920f25054be4ce5b10dbdec0
Amrith D S <amrith.ds.gowda@gmail.com>
2024-11-14 16:03:04 +0530

    fix

--- drivetest/course/helper/jobs/read_STM_job.py
+++ drivetest/course/helper/jobs/read_STM_job.py
@@ -31,9 +31,9 @@ def readSTMInputs():
                     #sensor_logger.info(data)
                     
                     # get distance from cache
-                    data[1] = cache.get('BACK_DISTANCE', 0)
+                    data[1] = cache.get('RIGHT_DISTANCE', 0)
                     data[2] = cache.get('LEFT_DISTANCE', 0)
-                    data[11] = cache.get('RIGHT_DISTANCE', 0)
+                    data[11] = cache.get('BACK_DISTANCE', 0)
                     sensor_logger.info(data)
                     # conside data less than 19 as noise
                     if len(data) == 19 and data != lastSensorFeed and CURRENT_REF_ID in RF_ID_OBSTACLE_MAP:
